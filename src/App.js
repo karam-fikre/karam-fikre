3d7e0fa956c1d10731269acf8a93f0770f25699d
karam-fikre <karamfikre@gmail.com>
2019-03-01 11:42:53 +0100

    Added video to the body on background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Header from './components/header.js';
-
+import bodyBackgroundVideo from './files/bodyBackgroundVideo.mp4';
 import './styles/App.css';
 
 class App extends Component {
+
 	render() {
 		return (
+			<div>
 				<Header />
+				<video className='bg'alt="bg" autoPlay loop muted>
+    		<source src={bodyBackgroundVideo} type='video/mp4' />
+				</video>
+			</div>
 		);
 	}
 }
